44518f7c50b83e562b024caa8ed2b943cfe824d0
readme916 <1290144599@qq.com>
2019-03-25 00:39:25 +0800

    1

--- src/main/java/com/liyang/jpa/smart/query/service/JdbcQueryService.java
+++ src/main/java/com/liyang/jpa/smart/query/service/JdbcQueryService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnBean(NamedParameterJdbcTemplate.class)
 public class JdbcQueryService {
 
 	private static NamedParameterJdbcTemplate jdbcTemplate;
