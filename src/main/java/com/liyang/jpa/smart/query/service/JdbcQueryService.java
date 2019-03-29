80152a919f4b58e95bafe3a945935dc88fb8859a
readme916 <1290144599@qq.com>
2019-03-29 22:44:15 +0800

    1

--- src/main/java/com/liyang/jpa/smart/query/service/JdbcQueryService.java
+++ src/main/java/com/liyang/jpa/smart/query/service/JdbcQueryService.java
@@ -3,16 +3,19 @@ package com.liyang.jpa.smart.query.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@ConditionalOnBean(NamedParameterJdbcTemplate.class)
-public class JdbcQueryService {
+public class JdbcQueryService implements ApplicationContextAware{
 
 	private static NamedParameterJdbcTemplate jdbcTemplate;
+	private static ApplicationContext applicationContext;
 	
 	@Autowired
 	public void setJdbcTemplate(NamedParameterJdbcTemplate jdbcTemplate) {
@@ -22,6 +25,18 @@ public class JdbcQueryService {
 
 
 	public static List<Map<String, Object>> query(String sql, Map<String, Object> preparedParams){
+		
+		if(jdbcTemplate==null) {
+			jdbcTemplate = JdbcQueryService.applicationContext.getBean(NamedParameterJdbcTemplate.class);
+		}
 		return  jdbcTemplate.queryForList(sql , preparedParams);
+	}
+
+
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		JdbcQueryService.applicationContext = applicationContext;
+		
 	}
 }
